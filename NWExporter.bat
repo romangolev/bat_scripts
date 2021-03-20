d26883752975eb8f665fac274773694e2bc3c516
Roman Golev <rv.golev@gmail.com>
2021-03-20 12:33:42 +0300

    add NWExporter

---
+++ NWExporter.bat
@@ -0,0 +1,4 @@
+@ECHO Navisworks Exporter 2019
+PAUSE
+"C:\Program Files\Autodesk\Navisworks Manage 2019\FiletoolsTaskRunner.exe" /i "D:\test\123.txt" /od "D:\test\123.txt" /inc /version 2019
+PAUSE
