9aa07793928a8faf05db074b5e7728e771e64bb0
Roman Golev <rv.golev@gmail.com>
2022-05-25 17:18:44 +0300

    Create NwcToNwd.bat
    add nwc to nwd bat script

---
+++ NwcToNwd.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Autodesk\Navisworks Manage 2020\Roamer.exe"  -nwd "C:\1\nwd\1.nwd"  "C:\1\nwc\1.nwc"
+"C:\Program Files\Autodesk\Navisworks Manage 2020\Roamer.exe"  -nwd "C:\1\nwd\2.nwd"  "C:\1\nwc\2.nwc"
+"C:\Program Files\Autodesk\Navisworks Manage 2020\Roamer.exe"  -nwd "C:\1\nwd\3.nwd"  "C:\1\nwc\3.nwc"
